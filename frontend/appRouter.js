c70987b111f8d18a8d204529db94ebbea8de0bdb
Ashirwad-Kushwaha <kushwahaashirwad1@gmail.com>
2024-07-26 04:20:20 +0530

    done

--- frontend/appRouter.js
+++ frontend/appRouter.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import LoginPage from './src/pages/loginPage';
 import SignupPage from './src/pages/SignupPage';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import HomePage from './src/pages/homePage';
 import OtpPage from './src/pages/otpPage';
 
 //const App = <h1>Hello</h1>
 
-const AppRouter=()=>{
-    const {isAuthorized, isEmailVerified} = useSelector((e)=>e.auth);
+const AppRouter = () => {
+    const { isAuthorized, isEmailVerified } = useSelector((e) => e.auth);
 
 
 
@@ -18,24 +18,24 @@ const AppRouter=()=>{
     const router = createBrowserRouter([
         {
             path: "/login",
-            element: isAuthorized ? <Navigate to="/"/> : <LoginPage/>
+            element: isAuthorized ? <Navigate to="/" /> : <LoginPage />
         },
         {
             path: "/signup",
-            element: isAuthorized ? <Navigate to="/" /> : <SignupPage/>
+            element: isAuthorized ? <Navigate to="/" /> : <SignupPage />
         },
         {
             path: "/otp",
-            element: isAuthorized && !isEmailVerified ? <OtpPage/> : <Navigate to="/" />
+            element: isAuthorized && !isEmailVerified ? <OtpPage /> : <Navigate to="/" />
 
         },
         {
             path: "/",
-            element: isAuthorized ? <>{isEmailVerified ? <HomePage/> : <Navigate to="/otp"/>}</> : <Navigate to="/login" />
+            element: isAuthorized ? <>{isEmailVerified ? <HomePage /> : <Navigate to="/otp" />}</> : <Navigate to="/login" />
         }
     ]);
 
-return <RouterProvider router={router}/>;
+    return <RouterProvider router={router} />;
 
 };
 
